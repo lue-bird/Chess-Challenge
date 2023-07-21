20b9170be02a5ba32e37c1baa6f566f2bb71f068
lue-bird <lue.the.bird@gmail.com>
2023-07-21 19:13:35 +0200

    very basic implementation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,48 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        return moves.MaxBy<Move, double>(move =>
+        {
+            board.MakeMove(move);
+            double evalAfterMove = BoardEvaluate(board);
+            board.UndoMove(move);
+            return evalAfterMove;
+        });
     }
+    // positive = white is better, negative = black is better
+    public double BoardEvaluate(Board board)
+    {
+        return
+            board.IsInCheckmate() ?
+                double.NegativeInfinity
+            : board.IsDraw() ?
+                0
+            :
+                BoardPieces(board).Sum(PieceEvaluate);
+    }
+    public double PieceEvaluate(Piece piece) =>
+        piece.PieceType switch
+        {
+            PieceType.King => 4,
+            PieceType.Queen => 8.6,
+            PieceType.Knight => 2.9,
+            PieceType.Bishop => 3.2,
+            PieceType.Rook => 4.5,
+            PieceType.Pawn => 1,
+            PieceType.None => 0
+        };
+
+    IEnumerable<Piece> BoardPieces(Board board) =>
+        BoardSquares.Select((square, i_) => board.GetPiece(square));
+
+
+    IEnumerable<Square> BoardSquares =
+        Enumerable.Range(0, 63).Select(i => new Square(i));
 }
